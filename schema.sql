a5c142e0fe385fec5c4e8c8773b33512d27fa8cc
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-22 16:58:20 -0600

    adding foreign keys and references to connect the tables within the database

--- schema.sql
+++ schema.sql
@@ -7,20 +7,27 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(30) NOT NULL
+    name VARCHAR(30)
 );
 
 CREATE TABLE roles (
     id INT PRIMARY KEY NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+
 );
 
 CREATE TABLE employees (
     id INT PRIMARY KEY NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
-)G
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+    FOREIGN KEY (manager_id)
+        REFERENCES employees(id)
+);
