407ee2d792b024013153f05d6ded0b10a72f8655
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-22 16:23:50 -0600

    seeded data into departments table

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO departments (id, name)
+VALUES
+    (001, "Sales"),
+    (002, "IT"),
+    (003, "Marketing"),
+    (004, "Data Engineering"),
+    (005, "Human Resources")
+    (006, "Finance"),
+    (007, "Customer Service"),
+    (008, "Operations");
+
+    SELECT * FROM departments;
+
+
+
