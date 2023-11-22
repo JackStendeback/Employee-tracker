9f9c336fbe64eb432c63c06ad043a2ada917ecda
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-22 16:40:56 -0600

    adding middleware and connecting to the database in server.js

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3002;
 const app = express();
+
+// Express Middleware required
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connecting to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'JackStendeback',
+        database: 'employee_db'
+    },
+    
+)
