c5e37d5d1bc5e7c14838432de0a6767f3aef41d6
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-22 16:07:39 -0600

    added department table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
+-- Make sure another db with the same name does not exist.
 DROP DATABASE IF EXISTS employee_db;
-
+-- Creating the database after checking to see if it already exists.
 CREATE DATABASE employee_db;
+-- Using the database after its creation.
+USE employee_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
