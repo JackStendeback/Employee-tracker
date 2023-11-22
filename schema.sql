6887161f1156eebd0fc40cb44b26f4485d79c823
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-22 16:48:06 -0600

    added not null to most schema.sql tables except for manager id since not everyone is a manager

--- schema.sql
+++ schema.sql
@@ -6,21 +6,21 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
 );
 
 CREATE TABLE employees (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
+    id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT
-)
+)G
