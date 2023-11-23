e5cf103e82520d70631f761b9c44f3eecdaaf00c
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-22 19:27:42 -0600

    added viewDepartment function to assist with inquirer

--- models/department.js
+++ models/department.js
@@ -4,7 +4,22 @@ const { table } = require('table');
 
 function viewDepartments() {
     return new Promise((resolve, reject) => {
-
+        const query = 'SELECT * FROM department';
+        connection.query(query, (err, results) => {
+            if (err) {
+                console.error('Could not fetch department data.');
+                reject(err);
+                return;
+            }
+            const data = [['Department ID', 'Department Name']];
+            results.forEach((department) => {
+                data.push([department.id, department.name]);
+            });
+            const output = table(data);
+            console.log('\nAll Departments');
+            console.log(output);
+            resolve();
+        })
     });
 }
 
