30a2c17fe21ce7c0a61d13d6e652bbb99580ecd9
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-23 22:02:55 -0600

    adding queryJS functionality connecting to server.js

---
+++ models/query.js
@@ -0,0 +1,15 @@
+
+const connection = require('../server.js');
+function executeQuery(query, params = []) {
+  return new Promise((resolve, reject) => {
+    connection.query(query, params, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
+module.exports = { executeQuery };
