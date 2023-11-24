cce3635c60981da9cc75136bba4a72f5ba4d08ae
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-23 21:16:47 -0600

    trying to make clean and slick query

--- models/employees.js
+++ models/employees.js
@@ -4,7 +4,15 @@ const { table } = require('table');
 
 function viewEmployees() {
     return new Promise((resolve, reject) => {
-
+        const query = `
+    SELECT
+        e.id AS 'EmployeeID',
+        CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
+        r.title AS 'Employee Role',
+        CONCAT(m.first_name, ' ', m.last_name) AS 'Manager Name'
+    FROM employees AS e
+    LEFT JOIN roles AS r ON e.role_id = r.id
+    LEFT JOIN employees AS m ON e.manager_id = m.id`;
     })
 }
 
@@ -16,6 +24,6 @@ function addEmployee() {
 
 function updateEmployee() {
     return new Promise((resolve, reject) =>  {
-        
+
     })
 }
