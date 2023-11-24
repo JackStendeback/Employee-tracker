6e06c98126b89addf90d32ad6b2fdab9ea7631d5
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-23 21:23:33 -0600

    adding rest of employee data functionality

--- models/employees.js
+++ models/employees.js
@@ -13,7 +13,29 @@ function viewEmployees() {
     FROM employees AS e
     LEFT JOIN roles AS r ON e.role_id = r.id
     LEFT JOIN employees AS m ON e.manager_id = m.id`;
-    })
+
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error('Error, could not fetch employees within database.', err);
+            return;
+        }
+
+        const employeeData = [['Employee ID', 'First Name', 'Last Name', 'Role', 'Manager']];
+        results.forEach((employee) => {
+            data.push([
+                employee['Employee ID'],
+                employee['First Name'],
+                employee['Last Name'],
+                employee['Role'],
+                employee['Manager']
+            ]);
+        });
+        const output = table(data);
+        console.log('\nAll Employees:');
+        console.log(output);
+        resolve();
+    });
+  });
 }
 
 function addEmployee() {
